3eeae3cfdf13ff331af820e442da42ba59da86ac
Chris Kerbo <chris.kerbo@outlook.com>
2020-03-24 18:57:32 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -2,3 +2,8 @@ function mapToNegativize(sourceArray) {
   const newArray = sourceArray.map(x => x * -1)
   return newArray
 }
+
+function mapToNoChange(sourceArray) {
+  const newArray = sourceArray.map(x => x)
+  return newArray
+}
